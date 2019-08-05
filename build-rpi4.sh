cc233ac71eea8ddbd579e2e7170f79391b5cf1bf
Satadru Pramanik <satadru@umich.edu>
2019-08-05 16:42:14 -0400

    debug wait

--- build-rpi4.sh
+++ build-rpi4.sh
@@ -147,7 +147,7 @@ wait_file() {
   local file="$1"; shift
   local wait_seconds="${1:-10}"; shift # 10 seconds as default timeout
 
-  until test ($((wait_seconds--)) -eq 0 -o -f "$file" ; do sleep 1; echo "${file}"; done
+  until test $((wait_seconds--)) -eq 0 -o -f "$file" ; do sleep 1; done
 
   ((++wait_seconds))
 }
