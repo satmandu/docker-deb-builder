e4b84be20910ca2714bb842a954df5b599b1352c
Satadru Pramanik <satadru@umich.edu>
2019-08-20 00:28:45 -0400

    Update build-rpi4.sh

--- build-rpi4.sh
+++ build-rpi4.sh
@@ -1471,13 +1471,13 @@ touch /flag/done.ok_to_exit_container_after_build
 [[ ! $JUSTDEBS ]] && non-free_firmware & 
 [[ ! $JUSTDEBS ]] && rpi_userland &
 [[ ! $JUSTDEBS ]] && andrei_gherzan_uboot_fork &
-kernelbuild_setup &
+kernelbuild_setup && kernel_debs &
 [[ ! $JUSTDEBS ]] && rpi_config_txt_configuration &
 [[ ! $JUSTDEBS ]] && rpi_cmdline_txt_configuration &
 [[ ! $JUSTDEBS ]] && wifi_firmware_modification &
 [[ ! $JUSTDEBS ]] && first_boot_scripts_setup &
 [[ ! $JUSTDEBS ]] && added_scripts &
-waitforstart "kernelbuild_setup" && kernel_debs &
+#waitforstart "kernelbuild_setup" && kernel_debs &
 [[ $BUILDNATIVE || ! $JUSTDEBS ]] && arm64_chroot_setup &
 [[ $BUILDNATIVE || ! $JUSTDEBS ]] && image_apt_installs &
 [[ $BUILDNATIVE || ! $JUSTDEBS ]] && spinnerwait image_apt_installs
