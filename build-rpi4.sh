42030ba64eaaec546a9507008ec6bdf4fc0dbb2f
Satadru Pramanik <satadru@umich.edu>
2019-08-20 00:20:50 -0400

    Update build-rpi4.sh

--- build-rpi4.sh
+++ build-rpi4.sh
@@ -138,7 +138,7 @@ wait_file() {
 #             PrintLog "file: ${file}, seconds: ${wait_seconds}" /tmp/wait_file.log
 #             sleep 1
 #         done
-    timeout ${wait_seconds} tail -F ${file} &> /dev/null
+    timeout ${wait_seconds} tail -f --retry ${file} &> /dev/null
   # [[ -f "${file}" ]] && PrintLog "${file} found at T-${wait_seconds} seconds." /tmp/wait_file.log
 #   [[ ${wait_seconds} -eq 0 ]] && PrintLog \
 #   "${file} hit time limit at ${wait_seconds} seconds." /tmp/wait_file.log
