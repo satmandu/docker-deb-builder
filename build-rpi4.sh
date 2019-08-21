db7952b6e1d27136ba278f19175e1a509ab9440a
Satadru Pramanik <satadru@umich.edu>
2019-08-21 15:35:43 -0400

    Update build-rpi4.sh

--- build-rpi4.sh
+++ build-rpi4.sh
@@ -506,9 +506,11 @@ VERSION_CODENAME=$(grep VERSION_CODENAME /etc/os-release | head -1 | awk -F '=' 
 [[ $BUILDNATIVE ]] && (
 echo "deb [arch=arm64] http://ports.ubuntu.com/ubuntu-ports ${VERSION_CODENAME} main restricted universe multiverse" >> /etc/apt/sources.list && \
 echo "deb [arch=arm64] http://ports.ubuntu.com/ubuntu-ports ${VERSION_CODENAME}-updates main restricted universe multiverse" >> /etc/apt/sources.list && \
- dpkg --add-architecture arm64 \
-        &>> /tmp/${FUNCNAME[0]}.install.log && \
-        ( apt update || true ) && \
+ dpkg --add-architecture arm64 
+        &>> /tmp/${FUNCNAME[0]}.install.log 
+        )
+[[ $BUILDNATIVE ]] && ( apt update || true ) 
+[[ $BUILDNATIVE ]] && (
     apt -o dir::cache::archives=${apt_cache} \
     -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" \
     install -y \
