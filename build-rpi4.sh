3e3f269bd92167d1414045a84e8f2eabb4cae7b4
Satadru Pramanik <satadru@umich.edu>
2019-08-21 15:37:34 -0400

    Update build-rpi4.sh

--- build-rpi4.sh
+++ build-rpi4.sh
@@ -509,7 +509,7 @@ echo "deb [arch=arm64] http://ports.ubuntu.com/ubuntu-ports ${VERSION_CODENAME}-
  dpkg --add-architecture arm64 
         &>> /tmp/${FUNCNAME[0]}.install.log 
         )
-[[ $BUILDNATIVE ]] && ( apt update || true ) 
+[[ $BUILDNATIVE ]] && ( apt update -qq &>> /tmp/${FUNCNAME[0]}.install.log || true ) 
 [[ $BUILDNATIVE ]] && (
     apt -o dir::cache::archives=${apt_cache} \
     -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" \
