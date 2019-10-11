bc750491a1117315754699915a787bae6b278a05
Satadru Pramanik <satadru@umich.edu>
2019-10-11 18:38:34 -0400

    Update taillog.sh

--- scripts/taillog.sh
+++ scripts/taillog.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-tail -F /tmp/build.log --pid=$(cat /flag/main)| grep --line-buffered -v "waitfor(): sleep" -v "waitfor(): wait_proc=" -v "waitfor(): echo \'scale=2" -v "waitfor(): bc"
+tail -F /tmp/build.log --pid=$(cat /flag/main)| grep --line-buffered -v "waitfor(): sleep" -v "waitfor(): wait_proc" -v "waitfor(): echo \'scale" -v "waitfor(): bc"
