966e420f179d391327a8b43d046b2621382e4eb9
Satadru Pramanik <satadru@umich.edu>
2019-10-11 18:45:16 -0400

    Update taillog.sh

--- scripts/taillog.sh
+++ scripts/taillog.sh
@@ -4,9 +4,9 @@ grep --line-buffered -v \
 -e "waitfor(): sleep" \
 -e "waitfor(): wait_proc" \
 -e "waitfor(): echo 'scale" \
--e "waitfor(): bc"
--e "PrintLog(): ts"
--e "wait_file(): grep -q -m1 ''"
--e "PrintLog(): local logFile=/tmp/wait.log"
--e "PrintLog(): [[ ! -e /tmp/wait.log ]]"
+-e "waitfor(): bc" \
+-e "PrintLog(): ts" \
+-e "wait_file(): grep -q -m1 ''" \
+-e "PrintLog(): local logFile=/tmp/wait.log" \
+-e "PrintLog(): [[ ! -e /tmp/wait.log ]]" \
 -e "PrintLog(): [[ -e /tmp/wait.log ]]"
