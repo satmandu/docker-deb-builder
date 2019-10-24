1f27566c4b73955295cef2b4b9fa18fc20765833
Satadru Pramanik <satadru@umich.edu>
2019-10-23 20:10:07 -0400

    Update in-image-script.sh

--- in-image-script.sh
+++ in-image-script.sh
@@ -901,8 +901,8 @@ startfunc
     else
         echo "  eth led 4.19 patch applied successfully!"
     fi)
-#     [[ $NOETHLED ]] && ( sed -i 's/BCM5482_SHD_SSD_LEDM/~BCM5482_SHD_SSD_LEDM/' "${workdir}"/rpi-linux/drivers/net/phy/broadcom.c || true ) 
-     [[ $NOETHLED ]] && ( sed -i '/^BCM5482_SHD_SSD_LEDM/d' "${workdir}"/rpi-linux/drivers/net/phy/broadcom.c || true ) 
+     [[ $NOETHLED ]] && ( sed -i 's/BCM5482_SHD_SSD_LEDM/~BCM5482_SHD_SSD_LEDM/' "${workdir}"/rpi-linux/drivers/net/phy/broadcom.c || true ) 
+#     [[ $NOETHLED ]] && ( sed -i '/^BCM5482_SHD_SSD_LEDM/d' "${workdir}"/rpi-linux/drivers/net/phy/broadcom.c || true ) 
     if [[ -e /tmp/APPLIED_KERNEL_PATCHES ]]
         then
             KERNEL_VERS="${PKGVER}${CONFIG_LOCALVERSION}-g${KERNELREV}$(< /tmp/APPLIED_KERNEL_PATCHES)"
